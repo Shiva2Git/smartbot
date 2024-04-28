8d54524dab8111689116823556c515ed38e2e61e
shiva <119168490+Shiva2Git@users.noreply.github.com>
2024-04-28 17:54:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,10 +116,11 @@ def handle_userinput(user_questions):
     st.session_state.chat_history = response['chat_history']
 
     for i, message in enumerate(st.session_state.chat_history):
+        
        if i % 2 == 0:
-           st.write(user_template.replace("{{MSG}}", message['text']), unsafe_allow_html=True)
+           st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
        else:
-           st.write(bot_template.replace("{{MSG}}", message['text']), unsafe_allow_html=True)
+           st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
     
 
 def main():
